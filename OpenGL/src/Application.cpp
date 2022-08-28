ece587dd78ad7e5cc3265eb011562862e18b0f15
Aviral <Aviral@DESKTOP-KP5LJ5G>
2022-08-28 13:37:43 -0700

    first shader compiled and run

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,6 +2,54 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr); // https://registry.khronos.org/OpenGL-Refpages/gl4/html/glShaderSource.xhtml
+    glCompileShader(id);
+    // TODO:  Error Handling
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << ((type == GL_VERTEX_SHADER) ? "Vertex" : "Fragment") << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+
+    }
+    return id;
+
+    
+
+}
+static int CreateShader(const std::string &vertexShader, const std::string &fragmentShader)
+{   
+    // glCreateProgram creates an empty program object and returns a non-zero value by which it can be referenced. 
+    // A program object is an object to which shader objects can be attached. 
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    // glValidateProgram checks to see whether the executables contained in program can execute given the current OpenGL state.
+    glValidateProgram(program);
+    // This command effectively undoes the effects of a call to glCreateShader.
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+   
+    return program;
+
+}
 
 int main(void)
 {
@@ -41,6 +89,23 @@ int main(void)
     // attribute index, num of values, type of values, no normalization, size of attribute, distance from first attribute
     glVertexAttribPointer(0, 2, GL_FLOAT,GL_FALSE,sizeof(float)*2,0); // create attribute buffer
     glEnableVertexAttribArray(0); // enable the state
+
+    std::string fragmentShader = "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "color = vec4(0.3f,0.01f, 0.2f,1.0);\n"
+        "}\n";
+    std::string vertexShader = "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "gl_Position = position;\n"
+        "}\n";
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
