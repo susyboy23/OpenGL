ceb76f7827f01cee55d5934d108492a22e5119fd
Aviral <Aviral@DESKTOP-KP5LJ5G>
2022-08-28 10:52:11 -0700

    better comments

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -26,15 +26,21 @@ int main(void)
     {
         std::cout << "Error\n";
     }
+    // info based on which triangles are to be drawn
     float positions[6] = {
-            -0.5f, -0.5
+           1.0f, 0.0f
             ,0.0f, 0.5f
             ,0.5f, -0.5f
     };
     unsigned int buffer; // an int to store the ID of the created buffer object
     glGenBuffers(1, &buffer); // create a buffer and store its ID into uint buffer
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); // declare that the created buffer object, with ID = buffer, is gonna be an array
+    // structure of the array, size of the buffer, starting point of the data, hint: gonna be used to draw infrequently
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // describe that array
+
+    // attribute index, num of values, type of values, no normalization, size of attribute, distance from first attribute
+    glVertexAttribPointer(0, 2, GL_FLOAT,GL_FALSE,sizeof(float)*2,0); // create attribute buffer
+    glEnableVertexAttribArray(0); // enable the state
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
